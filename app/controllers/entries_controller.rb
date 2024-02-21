a34a6da1c13fea21484f1d16bda79eef377bb7a9
Muazzam Gilani <muazzam.gilani@kellogg.northwestern.edu>
2024-02-21 22:30:11 +0000

    Final changes - was so close

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,7 +14,7 @@ class EntriesController < ApplicationController
 
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
-    @entry["date"] = params["date"]
+    @entry["posted_on"] = params["posted_on"]
 
     @entry["place_id"] = params["place_id"]
 
