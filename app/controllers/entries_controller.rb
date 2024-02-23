5a51b24703aec22f2eb14b4b20a8eec58d731483
Muazzam Gilani <muazzam.gilani@kellogg.northwestern.edu>
2024-02-23 01:34:35 +0000

    completed assignment

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,11 +1,7 @@
 class EntriesController < ApplicationController
 
-  def show
-    @entry = Entry.find_by({"id" => params["id"]})
-    @place = Place.find_by({"id" => @contact["place_id"]})
-  end
-
   def new
+    @entry = Entry.new
     @place = Place.find_by({ "id" => params["place_id"] })
   end
 
